4f10311f377fc7855a863ab2f42516a385510cb2
Jonathan Fung <16733034+jonathanfung@users.noreply.github.com>
2022-12-05 22:35:55 +0000

    feat: Day 5 - even smaller fixes

--- typescript/src/day-5/day-5-pt-2.ts
+++ typescript/src/day-5/day-5-pt-2.ts
@@ -4,7 +4,7 @@ type CrateStacks = { [stackNum: string]: string[] };
 
 const stacks: CrateStacks = {};
 
-const processStackLayout = (stacks: CrateStacks, inputLine: string) => {
+const processStackLayout = (stacks: CrateStacks, inputLine: string): void => {
   const matches = [...inputLine.matchAll(/\[([^\]]+)\]/g)];
   matches.forEach((each) => {
     const [_, match] = each;
@@ -17,11 +17,9 @@ const processStackLayout = (stacks: CrateStacks, inputLine: string) => {
       stacks[stackIndex] = [match];
     }
   });
-
-  return stacks;
 };
 
-const processMoveInstructionCrateMover9001 = (stacks: CrateStacks, inputLine: string) => {
+const processMoveInstructionCrateMover9001 = (stacks: CrateStacks, inputLine: string): void => {
   const regex = /move (?<quantity>\d+) from (?<from>\d+) to (?<to>\d+)/g;
   const matches = [...inputLine.matchAll(regex)];
 
