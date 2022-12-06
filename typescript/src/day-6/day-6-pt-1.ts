3c5849a912642c19b2fb62ee8cb97380b92aa4d5
Jonathan Fung <16733034+jonathanfung@users.noreply.github.com>
2022-12-06 23:01:18 +0000

    feat: Day 6 solutions in TS (#14)

---
+++ typescript/src/day-6/day-6-pt-1.ts
@@ -0,0 +1,24 @@
+import { INPUT, TEST1, TEST2, TEST3, TEST4 } from './input';
+
+const input = INPUT;
+
+const checkMarker = (buffer: string[]) => new Set<string>(buffer).size === 4;
+
+const detectMarkerPosition = () => {
+  const buffer: string[] = [...input.slice(0, 4)];
+
+  if (checkMarker(buffer)) {
+    return 4;
+  }
+
+  for (let i = 4; i < input.length; i++) {
+    buffer.shift();
+    buffer.push(input.charAt(i));
+
+    if (checkMarker(buffer)) {
+      return i + 1;
+    }
+  }
+};
+
+console.log(detectMarkerPosition());
